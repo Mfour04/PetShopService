16328224e265056fe9264e73f9442b56f34dc902
nguyennhuloc2003 <locnhunguyen2003@gmail.com>
2024-09-22 21:46:51 +0700

    Fix UI

---
+++ WebRazor/Pages/Checkout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebRazor.Pages
+{
+    public class CheckoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebRazor/Pages/Service-details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebRazor.Pages
+{
+    public class Service_detailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
